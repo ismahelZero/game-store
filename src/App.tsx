44549081438186be2b61304e1a81c4a488bde742
ismahelZero <ismahel.zero94@gmail.com>
2025-03-16 09:33:13 +0300

    upd: making list component to reusable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import ListGroup from "./components/ListGroup";
+import ListGroup from './components/ListGroup'
 
 function App() {
-    return <div><ListGroup/></div>
+  let items = [
+    'An item',
+    'A second item',
+    'A third item',
+    'A fourth item',
+    'And a fifth one'
+  ]
+
+  return (
+    <div>
+      <ListGroup items={items} heading="list of items" />
+    </div>
+  )
 }
 
 export default App
